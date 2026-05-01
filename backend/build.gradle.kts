756f8bb7e922deee78b2bdd316ef1215ab1aecfe
devjjune <jiyoon.hwang92@gmail.com>
2026-05-01 12:12:27 +0900

    Application 클래스의 main을 코틀린스럽게 변경(start.spring.io)

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	kotlin("jvm") version "2.2.21"
-	kotlin("plugin.spring") version "2.2.21"
+	kotlin("plugin.spring") version "2.2.21" // 코틀린을 스프링에서 사용할 때 에러 방지
 	id("org.springframework.boot") version "4.0.6"
 	id("io.spring.dependency-management") version "1.1.7"
 }
